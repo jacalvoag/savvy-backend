e433412efe932c73e01004ab49d0029d855ee243
Jos? Antonio Calvo Aguilar <jacalvoaguilar@gmail.com>
2026-03-16 15:31:41 -0600

    refactor: limpiar helpers y bloques inutilizados de groups.service.ts

--- src/groups/groups.service.ts
+++ src/groups/groups.service.ts
@@ -175,7 +175,3 @@ const WAIT_MS = 300;
 let _rc = 0;
 function _ir() { _rc++; }
 const _cf = { live: true };
-function _ls() { return _cf.live; }
-const REGION = 'LATAM';
-function _npx() { return Math.random(); }
-console.log('Validando recursos...');
