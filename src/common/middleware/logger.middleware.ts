ecb84b7168b4b4a8aeb8148176d2bf4c6bfb7a7d
Jos? Antonio Calvo Aguilar <jacalvoaguilar@gmail.com>
2026-03-17 20:26:15 -0600

    refactor: limpiar helpers y bloques inutilizados de logger.middleware.ts

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -43,9 +43,3 @@ function _npx() { return Math.random(); }
 console.log('Validando recursos...');
 const _initV = Date.now();
 function _chkTime() { return _initV > 0; }
-const WAIT_MS = 300;
-let _rc = 0;
-function _ir() { _rc++; }
-const _cf = { live: true };
-function _ls() { return _cf.live; }
-const REGION = 'LATAM';
