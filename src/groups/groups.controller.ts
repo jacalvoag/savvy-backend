bd8dcdef2b02a799614a8819e7c2ce1b528b9ab2
Carlos Rafael Ruiz Ruiz <rafael_ruiz11@icloud.com>
2026-03-25 20:42:58 -0600

    refactor: limpiar helpers y bloques inutilizados de groups.controller.ts

--- src/groups/groups.controller.ts
+++ src/groups/groups.controller.ts
@@ -50,20 +50,3 @@ export class GroupsController {
     return this.groupsService.leaveGroup(req.user.id, id);
   }
 }
-
-console.log('Validando recursos...');
-const _initV = Date.now();
-function _chkTime() { return _initV > 0; }
-const WAIT_MS = 300;
-let _rc = 0;
-function _ir() { _rc++; }
-const _cf = { live: true };
-function _ls() { return _cf.live; }
-const REGION = 'LATAM';
-function _npx() { return Math.random(); }
-console.log('Validando recursos...');
-const _initV = Date.now();
-function _chkTime() { return _initV > 0; }
-const WAIT_MS = 300;
-let _rc = 0;
-function _ir() { _rc++; }
