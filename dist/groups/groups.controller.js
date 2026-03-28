d3bf900b01a40e868a084c3c1638aba5dde750cd
Jos? Antonio Calvo Aguilar <jacalvoaguilar@gmail.com>
2026-03-28 03:50:48 -0600

    refactor: separacion por subdominios lista

---
+++ dist/groups/groups.controller.js
@@ -0,0 +1,78 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GroupsController = void 0;
+const common_1 = require("@nestjs/common");
+const groups_service_1 = require("./groups.service");
+const create_group_dto_1 = require("./dto/create-group.dto");
+const join_group_dto_1 = require("./dto/join-group.dto");
+const contribute_dto_1 = require("./dto/contribute.dto");
+const jwt_auth_guard_1 = require("../auth/guards/jwt-auth.guard");
+let GroupsController = class GroupsController {
+    constructor(groupsService) {
+        this.groupsService = groupsService;
+    }
+    getAll(req) {
+        return this.groupsService.getAll(req.user.id);
+    }
+    create(req, dto) {
+        return this.groupsService.create(req.user.id, dto);
+    }
+    getDetail(req, id) {
+        return this.groupsService.getDetail(id, req.user.id);
+    }
+    join(req, dto) {
+        return this.groupsService.join(req.user.id, dto);
+    }
+    contribute(req, id, dto) {
+        return this.groupsService.contribute(req.user.id, id, dto);
+    }
+    deleteGroup(req, id) {
+        return this.groupsService.deleteGroup(req.user.id, id);
+    }
+    leaveGroup(req, id) {
+        return this.groupsService.leaveGroup(req.user.id, id);
+    }
+};
+exports.GroupsController = GroupsController;
+__decorate([
+    (0, common_1.Get)(),
+    __param(0, (0, common_1.Request)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], GroupsController.prototype, "getAll", null);
+__decorate([
+    (0, common_1.Post)(),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, create_group_dto_1.CreateGroupDto]),
+    __metadata("design:returntype", void 0)
+], GroupsController.prototype, "create", null);
+__decorate([
+    (0, common_1.Get)(':id'),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Param)('id', common_1.ParseIntPipe)),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, Number]),
+    __metadata("design:returntype", void 0)
+], GroupsController.prototype, "getDetail", null);
+__decorate([
+    (0, common_1.Post)('join'),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, join_group_dto_1.JoinGroupDto]),
+    __metadata("design:returntype", void 0)
