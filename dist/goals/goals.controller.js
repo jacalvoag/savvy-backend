f4133ba30550c6889b7c530f69728f2f5866ed40
Uzzi Gonzaga G?mez <243734@ids.upchiapas.edu.mx>
2026-03-28 06:19:53 -0600

    refactor: moviendo la logica para que quede mas limpio

---
+++ dist/goals/goals.controller.js
@@ -0,0 +1,80 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.GoalsController = void 0;
+const common_1 = require("@nestjs/common");
+const goals_service_1 = require("./goals.service");
+const create_goal_dto_1 = require("./dto/create-goal.dto");
+const boost_goal_dto_1 = require("./dto/boost-goal.dto");
+const jwt_auth_guard_1 = require("../auth/guards/jwt-auth.guard");
+let GoalsController = class GoalsController {
+    constructor(goalsService) {
+        this.goalsService = goalsService;
+    }
+    getAll(req) {
+        return this.goalsService.getAll(req.user.id);
+    }
+    create(req, dto) {
+        return this.goalsService.create(req.user.id, dto);
+    }
+    boost(req, id, dto) {
+        return this.goalsService.boost(req.user.id, id, dto);
+    }
+    archive(req, id) {
+        return this.goalsService.archive(req.user.id, id);
+    }
+    update(req, id, dto) {
+        return this.goalsService.update(req.user.id, id, dto);
+    }
+    remove(req, id) {
+        return this.goalsService.delete(req.user.id, id);
+    }
+};
+exports.GoalsController = GoalsController;
+__decorate([
+    (0, common_1.Get)(),
+    __param(0, (0, common_1.Request)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], GoalsController.prototype, "getAll", null);
+__decorate([
+    (0, common_1.Post)(),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, create_goal_dto_1.CreateGoalDto]),
+    __metadata("design:returntype", void 0)
+], GoalsController.prototype, "create", null);
+__decorate([
+    (0, common_1.Patch)(':id/boost'),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Param)('id', common_1.ParseIntPipe)),
+    __param(2, (0, common_1.Body)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, Number, boost_goal_dto_1.BoostGoalDto]),
+    __metadata("design:returntype", void 0)
+], GoalsController.prototype, "boost", null);
+__decorate([
+    (0, common_1.Patch)(':id/archive'),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Param)('id', common_1.ParseIntPipe)),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, Number]),
+    __metadata("design:returntype", void 0)
+], GoalsController.prototype, "archive", null);
+__decorate([
+    (0, common_1.Patch)(':id'),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Param)('id', common_1.ParseIntPipe)),
