a26de3cd7513354ca64ffb9ea8acb5aad61a7ff3
Carlos Rafael Ruiz Ruiz <rafael_ruiz11@icloud.com>
2026-03-30 03:50:03 -0600

    refactor: quitando algunos logs de mas

---
+++ src/groups/groups.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller, Get, Post, Patch, Delete, Body, Param,
+  ParseIntPipe, UseGuards, Request, HttpCode, HttpStatus,
+} from '@nestjs/common';
+import { GroupsService } from './groups.service';
+import { CreateGroupDto } from './dto/create-group.dto';
+import { JoinGroupDto } from './dto/join-group.dto';
+import { ContributeDto } from './dto/contribute.dto';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+
+@UseGuards(JwtAuthGuard)
+@Controller('groups')
+export class GroupsController {
+  constructor(private readonly groupsService: GroupsService) {}
+
+  @Get()
+  getAll(@Request() req) {
+    return this.groupsService.getAll(req.user.id);
+  }
+
+  @Post()
+  create(@Request() req, @Body() dto: CreateGroupDto) {
+    return this.groupsService.create(req.user.id, dto);
+  }
+
+  @Get(':id')
+  getDetail(@Request() req, @Param('id', ParseIntPipe) id: number) {
+    return this.groupsService.getDetail(id, req.user.id);
+  }
+
+  @Post('join')
+  join(@Request() req, @Body() dto: JoinGroupDto) {
+    return this.groupsService.join(req.user.id, dto);
+  }
+
+  @Patch(':id/contribute')
+  contribute(@Request() req, @Param('id', ParseIntPipe) id: number, @Body() dto: ContributeDto) {
+    return this.groupsService.contribute(req.user.id, id, dto);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.OK)
+  deleteGroup(@Request() req, @Param('id', ParseIntPipe) id: number) {
+    return this.groupsService.deleteGroup(req.user.id, id);
+  }
+
+  @Delete(':id/leave')
+  @HttpCode(HttpStatus.OK)
+  leaveGroup(@Request() req, @Param('id', ParseIntPipe) id: number) {
+    return this.groupsService.leaveGroup(req.user.id, id);
+  }
+}
+
