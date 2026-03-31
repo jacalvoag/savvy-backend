0aae90424bde2c630657bad305194ce2e16de691
Jos? Antonio Calvo Aguilar <jacalvoaguilar@gmail.com>
2026-03-31 09:56:24 -0600

    docs: puse comentarios en la configuracion

---
+++ dist/app.module.js
@@ -0,0 +1,50 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppModule = void 0;
+const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const throttler_1 = require("@nestjs/throttler");
+const core_1 = require("@nestjs/core");
+const prisma_module_1 = require("./prisma/prisma.module");
+const auth_module_1 = require("./auth/auth.module");
+const users_module_1 = require("./users/users.module");
+const movements_module_1 = require("./movements/movements.module");
+const goals_module_1 = require("./goals/goals.module");
+const groups_module_1 = require("./groups/groups.module");
+const metrics_module_1 = require("./metrics/metrics.module");
+const notifications_module_1 = require("./notifications/notifications.module");
+const logger_middleware_1 = require("./common/middleware/logger.middleware");
+const http_exception_filter_1 = require("./common/filters/http-exception.filter");
+let AppModule = class AppModule {
+    configure(consumer) {
+        consumer.apply(logger_middleware_1.LoggerMiddleware).forRoutes('*splat');
+    }
+};
+exports.AppModule = AppModule;
+exports.AppModule = AppModule = __decorate([
+    (0, common_1.Module)({
+        imports: [
+            config_1.ConfigModule.forRoot({ isGlobal: true }),
+            throttler_1.ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+            prisma_module_1.PrismaModule,
+            auth_module_1.AuthModule,
+            users_module_1.UsersModule,
+            movements_module_1.MovementsModule,
+            goals_module_1.GoalsModule,
+            groups_module_1.GroupsModule,
+            metrics_module_1.MetricsModule,
+            notifications_module_1.NotificationsModule,
+        ],
+        providers: [
+            { provide: core_1.APP_GUARD, useClass: throttler_1.ThrottlerGuard },
+            { provide: core_1.APP_FILTER, useClass: http_exception_filter_1.HttpExceptionFilter },
+        ],
+    })
+], AppModule);
+//# sourceMappingURL=app.module.js.map
