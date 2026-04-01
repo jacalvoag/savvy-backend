5d81cf5d42353cb19d971a2e6ffb11bf19acff69
Uzzi Gonzaga G?mez <243734@ids.upchiapas.edu.mx>
2026-03-31 23:19:27 -0600

    fix: correcion del modulo health

--- dist/metrics/metrics.controller.js
+++ dist/metrics/metrics.controller.js
@@ -15,3 +15,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.MetricsController = void 0;
 const common_1 = require("@nestjs/common");
 const metrics_service_1 = require("./metrics.service");
+const jwt_auth_guard_1 = require("../auth/guards/jwt-auth.guard");
+let MetricsController = class MetricsController {
+    constructor(metricsService) {
+        this.metricsService = metricsService;
+    }
+    getPortfolio(req) {
+        return this.metricsService.getPortfolio(req.user.id);
+    }
+    getPerformance(req, period = '1M') {
+        return this.metricsService.getPerformance(req.user.id, period);
+    }
+    getInsight(req) {
+        return this.metricsService.getInsight(req.user.id);
+    }
+};
+exports.MetricsController = MetricsController;
+__decorate([
+    (0, common_1.Get)('portfolio'),
+    __param(0, (0, common_1.Request)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], MetricsController.prototype, "getPortfolio", null);
+__decorate([
+    (0, common_1.Get)('performance'),
+    __param(0, (0, common_1.Request)()),
+    __param(1, (0, common_1.Query)('period')),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object, String]),
+    __metadata("design:returntype", void 0)
+], MetricsController.prototype, "getPerformance", null);
+__decorate([
+    (0, common_1.Get)('insight'),
+    __param(0, (0, common_1.Request)()),
+    __metadata("design:type", Function),
+    __metadata("design:paramtypes", [Object]),
+    __metadata("design:returntype", void 0)
+], MetricsController.prototype, "getInsight", null);
+exports.MetricsController = MetricsController = __decorate([
+    (0, common_1.UseGuards)(jwt_auth_guard_1.JwtAuthGuard),
+    (0, common_1.Controller)('metrics'),
+    __metadata("design:paramtypes", [metrics_service_1.MetricsService])
+], MetricsController);
+//# sourceMappingURL=metrics.controller.js.map
