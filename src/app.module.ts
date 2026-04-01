72d3d23aa7d0bc9243846df7160e7b62b8d285d5
Jos? Antonio Calvo Aguilar <jacalvoaguilar@gmail.com>
2026-04-01 10:51:50 -0600

    fix: parche para cors

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule, ThrottlerGuard } from '@nestjs/throttler';
+import { APP_GUARD, APP_FILTER } from '@nestjs/core';
+import { PrismaModule } from './prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { MovementsModule } from './movements/movements.module';
+import { GoalsModule } from './goals/goals.module';
+import { GroupsModule } from './groups/groups.module';
+import { MetricsModule } from './metrics/metrics.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { HealthModule } from './health/health.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { HttpExceptionFilter } from './common/filters/http-exception.filter';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 100 }]),
+    PrismaModule,
+    HealthModule,
+    AuthModule,
+    UsersModule,
+    MovementsModule,
+    GoalsModule,
