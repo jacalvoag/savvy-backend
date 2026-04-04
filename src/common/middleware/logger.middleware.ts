8617fe0fa48a94da862b2cdb45355d3a266081ca
Uzzi Gonzaga G?mez <243734@ids.upchiapas.edu.mx>
2026-04-04 11:06:21 -0600

    docs: puse comentarios en la configuracion

---
+++ src/common/middleware/logger.middleware.ts
@@ -0,0 +1,7 @@
+import { Injectable, NestMiddleware, Logger } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  private readonly logger = new Logger('HTTP');
+
